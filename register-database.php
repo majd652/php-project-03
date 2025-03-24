f386d2d0564eda9d995cf080db21e11249187fd1
majd652 <majdzain809@gmail.com>
2025-03-24 13:04:22 +0100

    linking the sign up in the database +  fixes in data base

--- register-database.php
+++ register-database.php
@@ -1,11 +1,13 @@
 <?php
 require "database/database.php";
 
+if (!isset($_POST["username"]) || !isset($_POST["wachtwoord"])) {
+    die("Error: Missing required fields.");
+}
 
 $hash = password_hash($_POST["wachtwoord"], PASSWORD_DEFAULT);
 
-
-$insert_user = $conn->prepare("INSERT INTO account (username,wachtwoord) VALUES (:username, :wachtwoord)");
+$insert_user = $conn->prepare("INSERT INTO account (username, wachtwoord) VALUES (:username, :wachtwoord)");
 $insert_user->bindParam(":username", $_POST['username']);
 $insert_user->bindParam(":wachtwoord", $hash);
 $insert_user->execute();
--- signup.php
+++ signup.php
@@ -3,23 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Document</title>
+    <title>Sign Up - Upost</title>
     <link rel="stylesheet" href="css\main.css">
 </head>
 <body class="signupBody">
 <div class="content">
-    <div class="">
-        <!-- <img src="twitter-logo.png" alt="Twitter Logo"> -->
-    </div>
     <div class="card">
         <h2>Create your account</h2>
-        <form class="signupForm" id="signup-form">
-            <input class="inputName" type="text" placeholder="Name" required>
-            <input class="inputMail" type="email" placeholder="Email" required>
-            <input class="inputPass" type="password" placeholder="Password" required>
+        <form class="signupForm" id="signup-form" action="register-database.php" method="POST">
+            <input class="inputName" type="text" name="username" placeholder="Name" required>
+            <input class="inputMail" type="email" name="email" placeholder="Email" required>
+            <input class="inputPass" type="password" name="wachtwoord" placeholder="Password" required>
             <button type="submit" class="signupButton">Sign Up</button>
         </form>
-        <p>Already have an account? <a href="#">Log in</a></p>
+        <p>Already have an account? <a href="login.php">Log in</a></p>
     </div>
 </div>
 </body>
