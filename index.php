ac62195b7dd89fb3c6b20e8db05e08b80d89916e
majd652 <majdzain809@gmail.com>
2025-03-24 13:49:21 +0100

    worked on login.php and linked it to the database

--- index.php
+++ index.php
@@ -7,21 +7,6 @@
         <link rel="stylesheet" href="css\main.css">
     </head>
     <body>
-        <div id="logo" class="logo">
-<!--            <img src="images/Screenshot 2025-02-27 161647.png" alt="Logo" width="70px">-->
-        </div>
-        <div class="container">
-            <div class="login-box">
-                <h2>Sign in to Upost</h2>
-               
-                <form  action="register-database.php" method="post"  id="login-form" class="login-form">
-                    <input id="username" type="text" placeholder="Phone, email, or username" required>
-                    <input id="wachtwoord" type="password" placeholder="Password" required>
-                    <button type="submit">Sign in</button>
-                </form>
-                <a href="#" class="forgot-password">Forgot password?</a>
-                <a href="signup.php" class="signup-link">Sign up for Upost</a>
-            </div>
-        </div>
+      <?php require "login.php";?>
     </body>
 </html>
--- login.php
+++ login.php
@@ -3,11 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Document</title>
+    <title>Login - Upost</title>
     <link rel="stylesheet" href="css\main.css">
 </head>
 <body>
-
-
+    <div id="logo" class="logo">
+        <!--<img src="images/Screenshot 2025-02-27 161647.png" alt="Logo" width="70px">-->
+    </div>
+    <div class="container">
+        <div class="login-box">
+            <h2>Sign in to Upost</h2>
+            <form action="login-check.php" method="POST" class="login-form">
+                <input type="text" name="username" id="username" placeholder="Phone, email, or username" required>
+                <input type="password" name="wachtwoord" id="wachtwoord" placeholder="Password" required>
+                <button type="submit">Sign in</button>
+            </form>
+            <a href="#" class="forgot-password">Forgot password?</a>
+            <a href="signup.php" class="signup-link">Sign up for Upost</a>
+        </div>
+    </div>
 </body>
 </html>
