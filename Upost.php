c75ba54cffa43b720ce3835b2fc0f3950c765c78
majd652 <majdzain809@gmail.com>
2025-04-01 11:58:19 +0200

    upload pic database

--- Upost.php
+++ Upost.php
@@ -5,19 +5,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>upost</title>
     <link rel="stylesheet" href="css\main2.css">
- </head>
- <body>
- <label class="image-container" for="fileInput">
-    <span class="upload-text">profile pic</span>
-    </label>
-    <input class="file" type="file" id="fileInput" accept="image/*">
-    
-     <div class="logout_container" >
-    <button claas="logout_button">
-        <a class="Log_out" href="index.php">Log Out</a>
-    </button>
+</head>
+<body>
+    <div class="container">
+        <div>
+            <label class="image-container" for="fileToUpload">
+                <?php
+               
+                require_once "database/database.php";
+                $stmt = $conn->query("SELECT * FROM images ORDER BY upload_date DESC LIMIT 1");
+                $image = $stmt->fetch();
+                if ($image) {
+                    echo '<img src="uploads/' . htmlspecialchars($image['file_name']) . '" alt="Profile Picture" class="profile-image">';
+                } else {
+                    echo '<span class="upload-text">profile pic</span>';
+                }
+                ?>
+            </label>   
+            <form class="upload-button" action="upload.php" method="post" enctype="multipart/form-data">
+                <input type="file" name="fileToUpload" id="fileToUpload" accept="image/*">
+            </form>
+        </div>
+        
+        <div class="logout_container">
+            <button class="logout_button">
+                <a class="Log_out" href="index.php">Log Out</a>
+            </button>
+        </div>
     </div>
-</div>
     <script src="main.js"></script>
 </body>
 </html>
